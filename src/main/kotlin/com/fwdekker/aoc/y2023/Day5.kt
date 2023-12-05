24236ef513b88df8a504596baa88d661558d8b10
Florine W. Dekker <florine@fwdekker.com>
2023-12-05 15:31:25 +0100

    Add day 5

---
+++ src/main/kotlin/com/fwdekker/aoc/y2023/Day5.kt
@@ -0,0 +1,64 @@
+package com.fwdekker.aoc.y2023
+
+import kotlin.math.max
+import kotlin.math.min
+
+
+fun main() {
+    val lines = readResource("/y2023/Day5.txt").lines().filter(String::isNotBlank)
+
+    // Part 1
+    val seeds = lines[0].split(": ")[1].parseLongs()
+    lines.drop(1)
+        .fold(seeds.associateWith { it }) { results, line ->
+            if (!line[0].isDigit()) results.values.associateWith { it }
+            else results.mapValues(line.asMapper())
+        }
+        .also { println("Part one: ${it.values.min()}") }
+
+    // Part 2
+    val seedRanges = seeds.chunked(2).map { it[0]..<it[0] + it[1] }
+    lines.drop(1)
+        .fold(seedRanges.associateWith { it }) { results, line ->
+            if (!line[0].isDigit()) results.values.associateWith { it }
+            else results.map(line.asRangeMapper()).reduce(Map<LongRange, LongRange>::plus)
+        }
+        .also { println("Part two: ${it.values.minOf(LongRange::first)}") }
+}
+
+
+fun String.asMapper(): (Map.Entry<Long, Long>) -> Long {
+    val (to, from, length) = this.parseLongs()
+
+    return { (key, oldTarget) -> if (key - from in 0..<length) key - from + to else oldTarget }
+}
+
+fun String.asRangeMapper(): (Map.Entry<LongRange, LongRange>) -> Map<LongRange, LongRange> {
+    val (to, from, length) = this.parseLongs()
+
+    return { (range, oldTarget) ->
+        if (range.first != oldTarget.first) {
+            mapOf(range to oldTarget)
+        } else {
+            val fromRange = (from..<from + length)
+            val applicable = range.intersect(fromRange)
+            val inapplicable = range.diff(fromRange)
+
+            inapplicable.associateWith { it }.toMutableMap()
+                .also { if (!applicable.isEmpty()) it[applicable] = applicable.shift(to - from) }
+        }
+    }
+}
+
+
+fun String.parseLongs(): List<Long> = this.split(' ').map { it.toLong() }
+
+fun LongRange.intersect(that: LongRange): LongRange =
+    (max(this.first, that.first)..min(this.last, that.last))
+        .let { if (it.isEmpty()) LongRange.EMPTY else it }
+
+fun LongRange.diff(that: LongRange): List<LongRange> =
+    listOf(this.first..min(this.last, that.first - 1), max(this.first, that.last + 1)..this.last)
+        .filterNot { it.isEmpty() }
+
+fun LongRange.shift(by: Long): LongRange = this.first + by..this.last + by
