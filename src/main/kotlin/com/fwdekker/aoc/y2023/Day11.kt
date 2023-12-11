f4b3945224935f70da8c6e5f3013b9c74677720e
Florine W. Dekker <florine@fwdekker.com>
2023-12-11 12:36:31 +0100

    Add day 11

---
+++ src/main/kotlin/com/fwdekker/aoc/y2023/Day11.kt
@@ -0,0 +1,27 @@
+package com.fwdekker.aoc.y2023
+
+import kotlin.math.max
+import kotlin.math.min
+
+
+fun main() {
+    val lines = readResource("/y2023/Day11.txt").lines().filter(String::isNotBlank)
+
+    val expandX = lines.indices.filter { x -> lines[x].all { it == '.' } }
+    val expandY = lines[0].indices.filter { y -> lines.all { it[y] == '.' } }
+    val galaxies = lines.flatMapIndexed { idx, line -> line.indices.filter { line[it] == '#' }.map { Pair(idx, it) } }
+    val pairs = galaxies.flatMap { a -> galaxies.map { b -> Pair(a, b) } }
+
+    // Part 1
+    println("Part one: ${pairs.sumOf { (a, b) -> a.distance(b, expandX, expandY, 2) } / 2}")
+
+    // Part 2
+    println("Part one: ${pairs.sumOf { (a, b) -> a.distance(b, expandX, expandY, 1000000) } / 2}")
+}
+
+
+private fun Int.distance(that: Int, expanded: List<Int>, weight: Long): Long =
+    (min(this, that)..<max(this, that)).sumOf { if (it in expanded) weight else 1L }
+
+private fun Pair<Int, Int>.distance(that: Pair<Int, Int>, expandX: List<Int>, expandY: List<Int>, weight: Long): Long =
+    this.first.distance(that.first, expandX, weight) + this.second.distance(that.second, expandY, weight)
