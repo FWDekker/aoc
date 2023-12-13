200a660db3ad21e85c0a0095ba0e6fcfe7afd2e6
Florine W. Dekker <florine@fwdekker.com>
2023-12-13 22:15:19 +0100

    Fix day 13

--- src/main/kotlin/com/fwdekker/aoc/y2023/Day13.kt
+++ src/main/kotlin/com/fwdekker/aoc/y2023/Day13.kt
@@ -1,55 +1,26 @@
 package com.fwdekker.aoc.y2023
 
-import kotlin.math.min
-
 
 fun main() {
     val patterns = readResource("/y2023/Day13.txt").split("\n\n").map { it.lines().filter(String::isNotBlank) }
 
     // Part 1
-    println("Part one: ${patterns.sumOf { it.value() }}")
+    println("Part one: ${patterns.sumOf { it.value(targetDiff = 0) }}")
 
-    // Part 2 (does not work because the puzzle is badly designed)
-    println("Part two: ${patterns.sumOf { it.fixSmudge().value() }}")
+    // Part 2
+    println("Part two: ${patterns.sumOf { it.value(targetDiff = 1) }}")
 }
 
 
-private fun List<String>.rowLines(): List<Int> =
-    (1..<this.size)
-        .filter { separator ->
-            val offsets = 0..<min(separator, this.size - separator)
+private fun List<String>.transpose(): List<String> =
+    this[0].indices.map { col -> joinToString(separator = "") { "${it[col]}" } }
 
-            offsets.all { offset -> this[separator - offset - 1] == this[separator + offset] }
-        }
+private fun List<String>.diff(a: Int, b: Int): Int =
+    if (a !in indices || b !in indices) 0
+    else this[a].zip(this[b]).count { it.first != it.second }
 
-private fun List<String>.colLines(): List<Int> =
-    (1..<this[0].length)
-        .filter { separator ->
-            val offsets = 0..<min(separator, this[0].length - separator)
+private fun List<String>.mirrors(targetDiff: Int = 0): List<Int> =
+    indices.filter { row -> indices.sumOf { offset -> diff(row - offset - 1, row + offset) } == targetDiff }
 
-            offsets.all { offset -> this.map { it[separator - offset - 1] } == this.map { it[separator + offset] } }
-        }
-
-private fun List<String>.value(): Int = rowLines().sumOf { it * 100 } + colLines().sum()
-
-private fun List<String>.fixSmudge(): List<String> {
-    val oldRowLines = rowLines()
-    val oldColLines = colLines()
-
-    for (row in this.indices) {
-        for (col in this[0].indices) {
-            val fixed = this.toMutableList()
-            fixed[row] = fixed[row].replaceRange(col..col, if (fixed[row][col] == '.') "#" else ".")
-
-            val newRowLines = fixed.rowLines()
-            if (newRowLines.any { it !in oldRowLines })
-                return fixed
-
-            val newColLines = fixed.colLines()
-            if (newColLines.any { it !in oldColLines })
-                return fixed
-        }
-    }
-
-    error("Could not find a smudge to fix.")
-}
+private fun List<String>.value(targetDiff: Int = 0): Int =
+    mirrors(targetDiff).sumOf { it * 100 } + transpose().mirrors(targetDiff).sum()
