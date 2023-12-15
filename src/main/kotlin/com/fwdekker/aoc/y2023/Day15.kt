c71f3b657b5bc46911e3ce93e7562df5279a1804
Florine W. Dekker <florine@fwdekker.com>
2023-12-15 13:39:12 +0100

    Improve day 15

--- src/main/kotlin/com/fwdekker/aoc/y2023/Day15.kt
+++ src/main/kotlin/com/fwdekker/aoc/y2023/Day15.kt
@@ -2,40 +2,32 @@ package com.fwdekker.aoc.y2023
 
 
 fun main() {
-    val ops = readResource("/y2023/Day15.txt").trim().split(',')
+    val instructions = readResource("/y2023/Day15.txt").trim().split(',')
 
     // Part 1
-    println("Part one: ${ops.sumOf { it.hash() }}")
+    println("Part one: ${instructions.sumOf { Label(it).hashCode() }}")
 
     // Part 2
-    // Kotlin's maps preserve insertion order by default
-    println("Part two: ${ops.fold(List<Box>(256) { emptyMap() }) { boxes, op -> boxes.run(op) }.power()}")
+    val boxes = mutableMapOf<Label, MutableMap<String, Int>>()  // Kotlin maps preserve insertion order
+    instructions.forEach { boxes.perform(it) }
+    println("Part two: ${boxes.power()}")
 }
 
 
-private typealias Box = Map<String, Int>
+private class Label(private val label: String) {
+    override fun hashCode(): Int = label.fold(0) { hash, char -> ((hash + char.code) * 17) % 256 }
 
-private fun String.hash(): Int = fold(0) { hash, char -> ((hash + char.code) * 17) % 256 }
-
-private fun List<Box>.run(op: String): List<Box> {
-    val (label, focalLength) = op.split('-', '=').let { Pair(it[0], it[1].toIntOrNull() ?: 0) }
-    val hash = label.hash()
-
-    return replace(hash, this[hash].let { if ('-' in op) it.withoutKey(label) else it.replace(label, focalLength) })
+    override fun equals(other: Any?) = other is Label && this.hashCode() == other.hashCode()
 }
 
-private fun List<Box>.power(): Int =
-    indices.sumOf { box -> (box + 1) * this[box].values.indexed().sumOf { (slot, focal) -> (slot + 1) * focal } }
+private fun MutableMap<Label, MutableMap<String, Int>>.perform(instruction: String) {
+    val (label, focal) = instruction.split('-', '=')
 
+    getOrPut(Label(label)) { mutableMapOf() }
+        .also { if ('-' in instruction) it.remove(label) else it[label] = focal.toInt() }
+}
 
-fun <T> List<T>.replace(index: Int, element: T): List<T> =
-    toMutableList().also { it[index] = element }
-
-fun <K, V> Map<K, V>.replace(key: K, value: V): Map<K, V> =
-    toMutableMap().also { it[key] = value }
-
-fun <K, V> Map<K, V>.withoutKey(key: K): Map<K, V> =
-    filterKeys { it != key }
-
-fun <T> Collection<T>.indexed(): List<Pair<Int, T>> =
-    toList().mapIndexed { idx, it -> Pair(idx, it) }
+private fun Map<Label, MutableMap<String, Int>>.power(): Int =
+    entries.sumOf { (label, box) ->
+        (label.hashCode() + 1) * box.values.mapIndexed { slot, focal -> (slot + 1) * focal }.sum()
+    }
